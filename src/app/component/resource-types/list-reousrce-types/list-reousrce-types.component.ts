f6c84628265507fe184b9f16cd050a6d493493c2
kareemallam <98554346+amriallam@users.noreply.github.com>
2023-05-27 16:25:41 +0300

    search resource type

--- src/app/component/resource-types/list-reousrce-types/list-reousrce-types.component.ts
+++ src/app/component/resource-types/list-reousrce-types/list-reousrce-types.component.ts
@@ -11,6 +11,8 @@ export class ListReousrceTypesComponent implements OnInit {
 
   constructor(private modal: NgbModal) { }
 
+  searchTerm: string = "";
+
   ResourceTypes: ResourceType[] = [
     { Id: "1", Name: "Maid", Description: "Maid rental for cleaning differnet households", Quantity: 30, thumbnail: "" },
     { Id: "2", Name: "Car", Description: "Car rental for devliery service", Quantity: 20, thumbnail: "" },
@@ -18,11 +20,22 @@ export class ListReousrceTypesComponent implements OnInit {
     { Id: "4", Name: "Airplane", Description: "Airplane rental for entertainment or delivery", Quantity: 5, thumbnail: "" }
   ];
 
+
   ngOnInit(): void { }
 
   openModal(ResourceType: ResourceType) {
     const modelRef = this.modal.open(ResourceTypeAttributeDetailsComponent, { centered: true })
     modelRef.componentInstance.ResourceType = ResourceType;
   }
+
+  matchesSearchTerm(rt: any): boolean {
+    if (!this.searchTerm) {
+      return true; // If no search term provided, show all resources
+    }
+    const name = rt.Name.toLowerCase();
+    const searchTerm = this.searchTerm.toLowerCase();
+    return name.includes(searchTerm); // Check if resource name includes the search term
+  }
+
 
 }
