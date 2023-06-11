624a34a44d5509b7954494533abac708da8a8e55
Amr Allam <amr.i.allam@hotmail.com>
2023-06-11 03:28:28 +0300

    sales ratio updated

--- src/app/component/dashboard/dashboard-components/sales-ratio/sales-ratio.component.ts
+++ src/app/component/dashboard/dashboard-components/sales-ratio/sales-ratio.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { isThisSecond } from 'date-fns';
 import {
   ApexAxisChartSeries,
   ApexChart,
@@ -28,18 +27,23 @@ export type salesChartOptions = {
   grid: ApexGrid;
 };
 
+interface Result {
+  [key: string]: {
+    name: string;
+    data: number[];
+  };
+}
+
 @Component({
   selector: 'app-sales-ratio',
   templateUrl: './sales-ratio.component.html'
 })
 export class SalesRatioComponent implements OnInit {
-  categoriesData: number[] = [];
-  seriesData: { name: string, data: number[] }[] = []
   @ViewChild("chart") chart: ChartComponent = Object.create(null);
   public salesChartOptions: Partial<salesChartOptions>;
   constructor(private measureService: MeasuresService) {
     this.salesChartOptions = {
-      series: this.seriesData,
+      series: [],
       chart: {
         fontFamily: 'Rubik,sans-serif',
         height: 250,
@@ -69,7 +73,7 @@ export class SalesRatioComponent implements OnInit {
         size: 3
       },
       xaxis: {
-        categories: this.categoriesData,
+        categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
       },
       tooltip: {
         theme: 'light'
@@ -78,11 +82,40 @@ export class SalesRatioComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const yearStart = `${new Date().getFullYear()}-01-01`;
-    const yearEnd = `${new Date().getFullYear() + 1}-01-01`;
-    this.measureService.GetResourceTypesSalesPerMonth(yearStart, yearEnd).subscribe(data => {
-      console.log(data.data);
-    })
+    let currentYear = new Date().getFullYear();
+    const yearStart = `${currentYear}-01-01`;
+    const yearEnd = `${currentYear}-12-31`;
+    this.updateSeries(yearStart, yearEnd);
+    this.measureService.dateSubject.subscribe((value) => {
+      let fromDate = `${value.fromDate.year}-01-01}`;
+      let toDate = `${value.toDate.year}-12-31`;
+      this.updateSeries(fromDate, toDate);
+    });
   }
 
+  private updateSeries(yearStart: string, yearEnd: string,) {
+    this.measureService.GetResourceTypesSalesPerMonth(yearStart, yearEnd).subscribe(data => {
+      const result: Result = {};
+      data.data.forEach(item => {
+        const resourceType = item.resourceType;
+        const month = item.month;
+        const totalPrice = item.totalPrice;
+
+        if (!result[resourceType]) {
+          result[resourceType] = {
+            name: resourceType,
+            data: []
+          };
+        }
+
+        while (result[resourceType].data.length < month) {
+          result[resourceType].data.push(0);
+        }
+
+        result[resourceType].data.push(totalPrice);
+      });
+      this.salesChartOptions.series = Object.values(result);
+    })
+  }
 }
+
