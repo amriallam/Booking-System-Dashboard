a8dde24e141bd5bd521e05e70f474545ab39ed63
Radwa Elgammal <elgammalr74@gmail.com>
2023-06-26 03:38:04 +0300

    add

--- src/app/component/models/resource-special-charactaristics.ts
+++ src/app/component/models/resource-special-charactaristics.ts
@@ -4,6 +4,7 @@ export class ResourceSpecialCharactaristics {
         public TotalCapacity: number,
         public AvailableCapacity: number,
         public resourceID: number,
+        public day :any,
         public ScheduleID?: number,
       ) { }
 }
